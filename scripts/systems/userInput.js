566adf4e364c8720260005467c28e1c9aa327559
Erik Hazzard <enoex1@gmail.com>
2014-08-05 02:06:50 -0700

    adds basic ios support (a little janky)

--- scripts/systems/userInput.js
+++ scripts/systems/userInput.js
@@ -10,21 +10,66 @@
 // For purposes of a tutorial, we'll just manually setup related system 
 // functionality here
 //
-// start it off screen
-var mousePosition = {x:-100, y: -100};
+function hasTouchEnabled() { return 'ontouchstart' in window || 'onmsgesturechange' in window; }
+
+// start it off screen for non touch devices
+var userInputPosition = {
+    x: -100,
+    y: -100,
+    deltaX: false,
+    deltaY: false
+};
+
+// start it in center for touch devices
+if(hasTouchEnabled){
+    userInputPosition = {
+        x: ECS.$canvas.width / 2,
+        y: ECS.$canvas.height / 2,
+        lastX: ECS.$canvas.width / 2,
+        lastY: ECS.$canvas.height / 2
+    };
+}
 
 // Setup mouse handling
 // --------------------------------------
-function getMousePosition(evt) {
+function updateMousePosition(evt) {
     var rect = ECS.$canvas.getBoundingClientRect();
-    return { x: evt.clientX - rect.left, y: evt.clientY - rect.top };
+    userInputPosition.x = evt.clientX - rect.left;
+    userInputPosition.y = evt.clientY - rect.top;
+    userInputPosition.touch = false;
 }
 
-ECS.$canvas.addEventListener('mousemove', function(evt) {
-    // update the mouse position when moved
-    mousePosition = getMousePosition(evt);
+ECS.$canvas.addEventListener('mousemove', function mouseMove (evt) {
+    //// update the mouse position when moved
+    updateMousePosition(evt);
 }, false);
 
+// Setup hammer2.0 events
+// --------------------------------------
+var mc = new Hammer.Manager(ECS.$canvas);
+if(hasTouchEnabled()){
+    mc.add(new Hammer.Pan({ threshold: 0, pointers: 0 }));
+    mc.on("panstart", function onPanStart(ev){
+        var rect = ECS.$canvas.getBoundingClientRect();
+
+        userInputPosition.lastX = userInputPosition.x;
+        userInputPosition.lastY = userInputPosition.y;
+
+        userInputPosition.x = ev.center.x - rect.left - 10;
+        userInputPosition.y = ev.center.y - rect.top - 10;
+    });
+
+    mc.on("panmove", function onPanMove(ev) {
+        // For touch events, don't use x/y
+        userInputPosition.x = userInputPosition.lastX + ev.deltaX;
+        userInputPosition.y = userInputPosition.lastY + ev.deltaY;
+    });
+
+    mc.on("panend", function onPanEnd(ev){
+        //userInputPosition.lastX = userInputPosition.x;
+        //userInputPosition.lastY = userInputPosition.y;
+    });
+}
 
 // Setup the system
 // --------------------------------------
@@ -43,8 +88,8 @@ ECS.systems.userInput = function systemUserInput ( entities ) {
         if( curEntity.components.playerControlled ){
             // We can change component data based on input, which cause other
             // systems (e.g., rendering) to be affected
-            curEntity.components.position.x = mousePosition.x;
-            curEntity.components.position.y = mousePosition.y;
+            curEntity.components.position.x = userInputPosition.x; 
+            curEntity.components.position.y = userInputPosition.y;
         }
     }
 };
